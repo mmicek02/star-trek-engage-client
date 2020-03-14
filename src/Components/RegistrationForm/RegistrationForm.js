d41c8e23b1980b4df7b61cbfd0f2ad6b2678e68d
mmicek02 <micekmichael@gmail.com>
2020-03-14 12:54:14 -0500

    updated registration

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -1,71 +1,99 @@
 import React, { Component } from 'react'
 import '../../style/main.css'
+import propTypes from 'prop-types';
+import ApiContext from '../../ApiContext';
 
-class RegistrationForm extends React.Component {
+class RegistrationForm extends Component {
     /* 
       This componet will render the form that allows a new user
       to create an account and submit that account to the server.
   
       They should then be prompted to login.
     */
+   static contextType = ApiContext;
+
    constructor(props) {
     super(props);
     this.state = {
-        userid: 4,
+        userid: '',
         username: '',
         password: '',
+        datejoined: '',
     }
-}
-
-updateUsername = (Username) => {
-  this.setState({
-      username: Username
-  })
-}
-
-updatePassword = (Password) => {
-  this.setState({
-      password: Password
-  })
-}
-
-handleSubmit = e => {
-  e.preventDefault();
-
-  const userInfo = {
-      userid: this.state.userid,
-      username: this.state.username,
-      password: this.state.password
   }
 
-  const url ='https://infinite-spire-80617.herokuapp.com/api/users';
-  const options = {
-      method: 'POST',
-      body: JSON.stringify(userInfo),
-      headers: {
-        'content-type': 'application/json'
-      }
-  };
+  updateUsername = (Username) => {
+    this.setState({
+        username: Username
+    })
+  }
 
-  fetch(url, options)
+  updatePassword = (Password) => {
+    this.setState({
+        password: Password
+    })
+  }
 
-  .then(res => {
-      if(!res.ok) {
-          throw new Error('Something went wrong, please try again later');
-      }
-      return res.json();
-  })
-  .then(resJson => {
-      this.context.characters.push(resJson)
-      this.props.history.push(`/api/users/${userInfo.userid}`)
-  })
-  .catch(err => {
-      this.setState({
-          error: err.message
-      })
-  })
-}
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const userInfo = {
+        userid: this.state.userid,
+        username: this.state.username,
+        password: this.state.password,
+        datejoined: new Date
+    }
+
+    const url ='https://infinite-spire-80617.herokuapp.com/api/users';
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(userInfo),
+        headers: {
+          'content-type': 'application/json'
+        }
+    };
+
+    fetch(url, options)
+
+    .then(res => {
+        if(!res.ok) {
+            throw new Error('Something went wrong, please try again later');
+        }
+        return res.json();
+    })
+    .then(resJson => {
+        this.context.characters.push(resJson)
+        this.props.history.push(`/`)
+    })
+    .catch(err => {
+        this.setState({
+            error: err.message
+        })
+    })
+  }
+
+  validateUsername() {
+    const username = this.state.username;
+    if (username.length === 0) {
+      return 'username is required';
+    } else if (username.length < 3) {
+      return 'username must be at least 3 characters long';
+    }
+  }
+
+  validatePassword() {
+    const password = this.state.password;
+    if (password.length === 0) {
+      return 'password is required';
+    } else if (password.length < 6) {
+      return 'password must be at least 6 characters long';
+    }
+  }
+
    render() {
+    console.log(this.state)
+    const nameError = this.validateUsername();
+    const passwordError = this.validatePassword();
     return (
       <form className='signup-form'>
       <div>
@@ -89,6 +117,12 @@ handleSubmit = e => {
         className="add-character-button"
         type='submit'
         onClick={e => this.handleSubmit(e)}
+        disabled = {
+          this.validateUsername(),
+          this.validatePassword(),
+          !!nameError,
+          !!passwordError
+        }
       >
           Sign Up</button>
   </form>
@@ -96,4 +130,9 @@ handleSubmit = e => {
   }
 }
 
-export default RegistrationForm
+export default RegistrationForm;
+
+RegistrationForm.propTypes = {
+  username: propTypes.string.isRequired,
+  password: propTypes.string.isRequired
+};
