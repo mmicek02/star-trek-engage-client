367f00a9cd2b9a22cd2633b425d8bbbd28f6d00b
mmicek02 <micekmichael@gmail.com>
2020-05-07 18:57:59 -0500

    updated code to included path to Api Context

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link, BrowserRouter } from 'react-router-dom'
 import TokenService from '../../Services/token-services'
+import ApiContext from '../../ApiContext'
 import '../../style/main.css'
 
 class Header extends Component {
