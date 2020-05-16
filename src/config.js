25e192b0dddb639e8ddfef5adb7a8c2c035f2745
mmicek02 <micekmichael@gmail.com>
2020-05-16 13:26:48 -0500

    fixed address for live push

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    //'https://infinite-spire-80617.herokuapp.com/api',
+    API_ENDPOINT: 'https://infinite-spire-80617.herokuapp.com/api',
     TOKEN_KEY: 'star-trek-client-auth-token',
 } 
